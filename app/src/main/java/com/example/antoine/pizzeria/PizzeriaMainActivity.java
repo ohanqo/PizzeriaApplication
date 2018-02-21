e22c9ba0ddece5bd46c8aa5e6286cb6c59d2b4a3
ohanqo <gm.anqo@gmail.com>
2018-02-21 11:19:12 +0100

    First commmit, restaurant tables selection

---
+++ app/src/main/java/com/example/antoine/pizzeria/PizzeriaMainActivity.java
@@ -0,0 +1,175 @@
+package com.example.antoine.pizzeria;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PizzeriaMainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnRoya, btnHawa, btnMont, btnFrom, btnNapo, btnRacl, btnPann, btnTira;
+    static int nbRoya, nbHawa, nbMont, nbFrom, nbNapo, nbRacl, nbPann, nbTira;
+
+    private TextView txtTabl;
+    private int numTabl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizzeria_main);
+
+        btnRoya = findViewById(R.id.btnRoya);
+        btnRoya.setOnClickListener(this);
+
+        btnHawa = findViewById(R.id.btnHawa);
+        btnHawa.setOnClickListener(this);
+
+        btnMont = findViewById(R.id.btnMont);
+        btnMont.setOnClickListener(this);
+
+        btnFrom = findViewById(R.id.btnFrom);
+        btnFrom.setOnClickListener(this);
+
+        btnNapo = findViewById(R.id.btnNapo);
+        btnNapo.setOnClickListener(this);
+
+        btnRacl = findViewById(R.id.btnRacl);
+        btnRacl.setOnClickListener(this);
+
+        btnPann = findViewById(R.id.btnPann);
+        btnPann.setOnClickListener(this);
+
+        btnTira = findViewById(R.id.btnTira);
+        btnTira.setOnClickListener(this);
+
+        Intent intent = getIntent();
+        txtTabl = findViewById(R.id.txtTabl);
+        numTabl = intent.getIntExtra(PizzeriaTableActivity.keyTabl, 1);
+        txtTabl.setText("Commande de la table n°" + numTabl);
+
+        if (savedInstanceState != null) {
+
+            int valRoya = savedInstanceState.getInt(getResources().getString(R.string.keyRoya));
+            int valHawa = savedInstanceState.getInt(getResources().getString(R.string.keyHawa));
+            int valMont = savedInstanceState.getInt(getResources().getString(R.string.keyMont));
+            int valFrom = savedInstanceState.getInt(getResources().getString(R.string.keyFrom));
+            int valNapo = savedInstanceState.getInt(getResources().getString(R.string.keyNapo));
+            int valRacl = savedInstanceState.getInt(getResources().getString(R.string.keyRacl));
+            int valPann = savedInstanceState.getInt(getResources().getString(R.string.keyPann));
+            int valTira = savedInstanceState.getInt(getResources().getString(R.string.keyTira));
+
+            btnRoya.setText("Royale : " + String.valueOf(valRoya));
+            btnHawa.setText("Hawai : " +  String.valueOf(valHawa));
+            btnMont.setText("Montagnarde : " + String.valueOf(valMont));
+            btnFrom.setText("Quatre Fromages : " + String.valueOf(valFrom));
+            btnNapo.setText("Napolitaine : " + String.valueOf(valNapo));
+            btnRacl.setText("Raclette : " + String.valueOf(valRacl));
+            btnPann.setText("Panna Cotta : " + String.valueOf(valPann));
+            btnTira.setText("Tiramisu : " + String.valueOf(valTira));
+
+        }
+    }
+
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnRoya:
+                nbRoya++;
+                btnRoya.setText("Royale : " + nbRoya);
+                System.out.println("Royale " + nbRoya);
+                break;
+            case R.id.btnHawa:
+                nbHawa++;
+                btnHawa.setText("Hawai : " + nbHawa);
+                System.out.println("Hawai " + nbHawa);
+                break;
+            case R.id.btnMont:
+                nbMont++;
+                btnMont.setText("Montagnarde : " + nbMont);
+                System.out.println("Montagnarde " + nbMont);
+                break;
+            case R.id.btnFrom:
+                nbFrom++;
+                btnFrom.setText("Quatre Fromages : " + nbFrom);
+                System.out.println("Quatre Fromages " + nbFrom);
+                break;
+            case R.id.btnNapo:
+                nbNapo++;
+                btnNapo.setText("Napolitaine : " + nbNapo);
+                System.out.println("Napolitaine " + nbNapo);
+                break;
+            case R.id.btnRacl:
+                nbRacl++;
+                btnRacl.setText("Raclette : " + nbRacl);
+                System.out.println("Raclette " + nbRacl);
+                break;
+            case R.id.btnPann:
+                nbPann++;
+                btnPann.setText("Panna Cotta : " + nbPann);
+                System.out.println("Panna Cotta " + nbPann);
+                break;
+            case R.id.btnTira:
+                nbTira++;
+                btnTira.setText("Tiramisu : " + nbTira);
+                System.out.println("Tiramisu " + nbTira);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(getResources().getString(R.string.keyRoya), nbRoya);
+        outState.putInt(getResources().getString(R.string.keyHawa), nbHawa);
+        outState.putInt(getResources().getString(R.string.keyMont), nbMont);
+        outState.putInt(getResources().getString(R.string.keyFrom), nbFrom);
+        outState.putInt(getResources().getString(R.string.keyNapo), nbNapo);
+        outState.putInt(getResources().getString(R.string.keyRacl), nbRacl);
+        outState.putInt(getResources().getString(R.string.keyPann), nbPann);
+        outState.putInt(getResources().getString(R.string.keyTira), nbTira);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("Éxécution -> onStart()");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("Éxécution -> onResume()");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("Éxécution -> onPause()");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("Éxécution -> onStop()");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("Éxécution -> onDestroy()");
+    }
+    /*
+    @Override
+    protected void onRestoreInstanceState(Bundle outState) {
+        super.onRestoreInstanceState(outState);
+        btnRoya.setText("Royale : " + outState.get(getResources().getString(R.string.keyRoya)));
+        btnHawa.setText("Hawai : " +  outState.get(getResources().getString(R.string.keyHawa)));
+        btnMont.setText("Montagnarde : " + outState.get(getResources().getString(R.string.keyMont)));
+        btnFrom.setText("Quatre Fromages : " + outState.get(getResources().getString(R.string.keyFrom)));
+        btnNapo.setText("Napolitaine : " + outState.get(getResources().getString(R.string.keyNapo)));
+        btnRacl.setText("Raclette : " + outState.get(getResources().getString(R.string.keyRacl)));
+        btnPann.setText("Panna Cotta : " + outState.get(getResources().getString(R.string.keyPann)));
+        btnTira.setText("Tiramisu : " + outState.get(getResources().getString(R.string.keyTira)));
+    }
+    */
+}
---
+++ app/src/main/java/com/example/antoine/pizzeria/PizzeriaTableActivity.java
@@ -0,0 +1,61 @@
+package com.example.antoine.pizzeria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+
+public class PizzeriaTableActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnVali;
+    private EditText iptTable;
+    static int idTable;
+    final static String keyTabl = "CLE_TABL";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pizzeria_table);
+
+        btnVali = findViewById(R.id.btnVali);
+        btnVali.setOnClickListener(this);
+        btnVali.setEnabled(false);
+
+        iptTable = findViewById(R.id.iptTabl);
+        iptTable.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(iptTable.getText() != null && !iptTable.getText().toString().matches("")) {
+                    btnVali.setEnabled(true);
+                } else {
+                    btnVali.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    public void onClick(View v) {
+        String tempId = String.valueOf(iptTable.getText());
+        idTable = Integer.parseInt(tempId);
+        Intent intent = new Intent(this, PizzeriaMainActivity.class);
+        intent.putExtra(keyTabl, idTable);
+        startActivity(intent);
+    }
+
+
+
+}
