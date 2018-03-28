b4b2e46394b8bb7021082756b455523b7fdb900e
ohanqo <gm.anqo@gmail.com>
2018-03-28 22:46:30 +0200

    Ajout du boutton préférences graphiquement

--- app/src/main/java/com/example/antoine/pizzeria/PizzeriaMainActivity.java
+++ app/src/main/java/com/example/antoine/pizzeria/PizzeriaMainActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -229,4 +232,18 @@ public class PizzeriaMainActivity extends AppCompatActivity implements PizzaFrag
         btnTira.setText("Tiramisu : " + outState.get(getResources().getString(R.string.keyTira)));
     }
     */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Afficher les settings
+        return super.onOptionsItemSelected(item);
+    }
 }
